92d699d3fe7f88d5567fed619c01f305e6a73540
Christian Schabesberger <chris.schabesberger@mailbox.org>
2026-05-15 12:53:50 +0200

    intermediate commit

--- IKEA_spiegelkugel.cpp
+++ IKEA_spiegelkugel.cpp
@@ -1,12 +1,32 @@
 #include "IKEA_spiegelkugel.h"
 
 
-L298NMotor::L298NMotor() {
+L298NMotor::L298NMotor(byte in1Pin, byte in2Pin, byte speedPin)
+  : in1Pin(in1Pin), in2Pin(in2Pin), speedPin(speedPin)
+{
+  PinManager::allocatePin(in1Pin, true, PinOwner::UM_Unspecified);
+  PinManager::allocatePin(in2Pin, true, PinOwner::UM_Unspecified);
+  PinManager::allocatePin(speedPin, true, PinOwner::UM_Unspecified);
 
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+
+#ifdef ARDUINO_ARCH_ESP32
+  pwmChannel = PinManager::allocateLedc(1);
+  ledcSetup(pwmChannel, 20000, 8);   // 20 kHz, 8-bit duty (0..255)
+  ledcAttachPin(speedPin, pwmChannel);
+#else
+  analogWriteRange(255);
+  analogWriteFreq(WLED_PWM_FREQ);
+  pinMode(speedPin, OUTPUT);
+#endif
 }
 
 void IKEA_spiegelkugel::setup()
 {
+  // TODO: replace placeholder GPIOs with the real wiring
+  openCloseMotor = new L298NMotor(/*in1*/ 25, /*in2*/ 26, /*speed*/ 27);
+  rotationMotor  = new L298NMotor(/*in1*/ 32, /*in2*/ 33, /*speed*/ 13);
 }
 
 void IKEA_spiegelkugel::loop()
--- IKEA_spiegelkugel.h
+++ IKEA_spiegelkugel.h
@@ -2,18 +2,24 @@
 #pragma once
 #include "wled.h"
 
-class IKEA_spiegelkugel : public Usermod
-{
-private:
-public:
-  void setup();
-  void loop();
-};
-
 class L298NMotor {
 private:
   byte in1Pin, in2Pin;
   byte speedPin;
+#ifdef ARDUINO_ARCH_ESP32
+  byte pwmChannel;
+#endif
 public:
   L298NMotor(byte in1Pin, byte in2Pin, byte speedPin);
+};
+
+
+class IKEA_spiegelkugel : public Usermod
+{
+private:
+  L298NMotor* openCloseMotor = nullptr;
+  L298NMotor* rotationMotor = nullptr;
+public:
+  void setup();
+  void loop();
 };
