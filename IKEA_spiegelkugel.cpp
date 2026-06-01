f9b78fae73ef43dcca6d5a0c198c4dfe9fca28dc
Christian Schabesberger <chris.schabesberger@mailbox.org>
2026-06-01 19:49:20 +0200

    hard set pins

--- IKEA_spiegelkugel.cpp
+++ IKEA_spiegelkugel.cpp
@@ -1,18 +1,10 @@
 #include "IKEA_spiegelkugel.h"
 #include "wled.h"
 
-namespace {
-// Open/close motor wiring
-constexpr byte OPEN_CLOSE_IN1_PIN = 25;
-constexpr byte OPEN_CLOSE_IN2_PIN = 26;
-
-// Rotation motor wiring
-constexpr byte ROTATION_IN1_PIN = 32;
-constexpr byte ROTATION_IN2_PIN = 33;
-} // namespace
-
 L298NMotor::L298NMotor(byte in1Pin, byte in2Pin)
-    : in1Pin(in1Pin), in2Pin(in2Pin) {
+    : in1Pin(in1Pin), in2Pin(in2Pin);
+
+void L298NMotor::setup() {
   PinManager::allocatePin(in1Pin, true, PinOwner::UM_Unspecified);
   PinManager::allocatePin(in2Pin, true, PinOwner::UM_Unspecified);
 
@@ -43,8 +35,8 @@ int8_t L298NMotor::getSpeed() {
 }
 
 void IKEA_spiegelkugel::setup() {
-  openCloseMotor = new L298NMotor(OPEN_CLOSE_IN1_PIN, OPEN_CLOSE_IN2_PIN);
-  rotationMotor  = new L298NMotor(ROTATION_IN1_PIN,   ROTATION_IN2_PIN);
+  openCloseMotor.setup();
+  rotationMotor.setup();
 }
 
 void IKEA_spiegelkugel::loop() { Serial.println("Hallo jan"); }
--- IKEA_spiegelkugel.h
+++ IKEA_spiegelkugel.h
@@ -2,6 +2,19 @@
 #pragma once
 #include "wled.h"
 
+
+namespace {
+// Open/close motor wiring
+constexpr byte OPEN_CLOSE_IN1_PIN = 25;
+constexpr byte OPEN_CLOSE_IN2_PIN = 26;
+
+// Rotation motor wiring
+constexpr byte ROTATION_IN1_PIN = 32;
+constexpr byte ROTATION_IN2_PIN = 33;
+} // namespace
+
+
+
 class L298NMotor {
 private:
   int16_t internal_speed;
@@ -11,6 +24,8 @@ private:
 public:
   L298NMotor(byte in1Pin, byte in2Pin);
 
+  void setup();
+
   void setSpeed(int8_t speed);
   int8_t getSpeed();
 };
@@ -19,8 +34,8 @@ public:
 class IKEA_spiegelkugel : public Usermod
 {
 private:
-  L298NMotor* openCloseMotor = nullptr;
-  L298NMotor* rotationMotor = nullptr;
+  L298NMotor openCloseMotor = L298NMotor(OPEN_CLOSE_IN1_PIN, OPEN_CLOSE_IN2_PIN);
+  L298NMotor rotationMotor = L298NMotor(ROTATION_IN1_PIN,   ROTATION_IN2_PIN);
 public:
   void setup();
   void loop();
